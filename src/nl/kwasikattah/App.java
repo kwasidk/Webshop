00d05b77023d7cfdc663fb81bc91ea73b16cd8b2
Kwasi Kattah <kattahkwasi@xs4all.nl>
2021-03-07 23:25:16 +0100

    Added SQL Statements so that you can register

--- src/nl/kwasikattah/App.java
+++ src/nl/kwasikattah/App.java
@@ -2,14 +2,16 @@ package nl.kwasikattah;
 
 public class App
 {
+
+    private static final String url = "jdbc:mysql://localhost:3306/Webshop";
+    private static final String username = "root";
+    private static final String password = "root";
+    public static final Database DATABASE = new Database(url, username, password);
+
     public static void main(String[] args) {
 
-        ConnectionDB connectionDB = new ConnectionDB();
-        connectionDB.Database();
-
-        Home home = new Home();
-        home.HomepageChoices();
-
+        Menu menu = new Menu();
+        menu.HomepageChoices();
 
     }
 
